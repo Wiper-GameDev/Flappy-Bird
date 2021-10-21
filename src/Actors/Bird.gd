8ee113f5b6124024b3d29a295a8af4abe5aece26
Wiper-R <rshivang12345@gmail.com>
2021-10-21 21:11:29 +0530

    Fix bird not colliding

--- src/Actors/Bird.gd
+++ src/Actors/Bird.gd
@@ -12,7 +12,6 @@ const ROTATION_MULTIPLIER = 5
 
 
 var _velocity := Vector2.ZERO
-var gravity  := 800.0
 
 func _ready() -> void:
 	animated_sprite.play("flap")
@@ -21,22 +20,17 @@ func _ready() -> void:
 	
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("flap"):
-		_velocity.y = -flap_impulse
+		linear_velocity.y = -flap_impulse
 		if wing_sound.playing:
 			wing_sound.stop()
 		wing_sound.play()
 		
 	animated_sprite.rotation_degrees = get_rotation_degrees()
-
-
-func _physics_process(delta: float) -> void:
-	_velocity.y += gravity * delta
-	position += _velocity * delta
 	
 	
 	
 func get_rotation_degrees():
-	return _velocity.y * get_physics_process_delta_time() * ROTATION_MULTIPLIER
+	return linear_velocity.y * get_physics_process_delta_time() * ROTATION_MULTIPLIER
 
 	
 	
