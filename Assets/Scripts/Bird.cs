43b0e0822df9450fd460a09ac19dc5659ef44cfd
Wiper-R <rshivang12345@gmail.com>
2023-05-10 13:08:29 +0530

    Bird animation and movement done

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -5,76 +5,110 @@ using UnityEngine.InputSystem;
 
 public class Bird : MonoBehaviour
 {
-    Rigidbody2D RB;
-    bool pressFlap = false;
-    bool flying = false;
-
+    // Serialized fields
     [SerializeField] float maxRotationUpward;
     [SerializeField] float maxRotationDownward;
-
     [SerializeField] float flapForce;
     [SerializeField] float maxFallSpeed;
+
+    // Components
+    Rigidbody2D RB;
     Animator animator;
 
+    // Flags
+    bool _pressedFlap = false;
+    bool _canRotate = false;
+    bool _canFlap = true;
+    bool _isDead = false;
+
+    // Constants
     const string IDLE_ANIM = "Idle";
     const string FLAP_ANIM = "Flap";
 
     void Awake()
     {
+        // Get components
         RB = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
+
     void Start()
     {
-
+        // Initialization code goes here
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Update the vertical velocity to clamp the fall speed
         RB.velocity = new Vector2(RB.velocity.x, Mathf.Max(RB.velocity.y, -maxFallSpeed));
 
+        // Play the appropriate animation based on the bird's orientation
+        if ((315 >= transform.eulerAngles.z && transform.eulerAngles.z >= 275) || !RB.simulated)
+            animator.Play(IDLE_ANIM);
+        else
+            animator.Play(FLAP_ANIM);
     }
-
-    void HandleRotation(){
-        // Rotation
-        if (RB.velocity.y >= 0f){
-            var target = maxRotationUpward - transform.rotation.z;
-            var computed = RB.velocity.y / flapForce * target;
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, computed), 0.5f);
-        }
-    }
-
-
 
     void FixedUpdate()
     {
-        if (pressFlap)
-        {
-            // Play Flap Animation
-            animator.Play(FLAP_ANIM);
-            var force = flapForce;
-            force -= RB.velocity.y;
-            RB.AddForce(Vector2.up * force, ForceMode2D.Impulse);
-        }
+        // Apply the flap force if the flap button was pressed and the bird is not dead
+        if (_pressedFlap && !_isDead)
+            ApplyFlap();
 
+        // Start simulating the bird if the flap button was pressed and the bird is not yet simulated
+        if (_pressedFlap && !RB.simulated)
+            StartSimulating();
 
-        if (pressFlap && !RB.simulated)
-        {
-            RB.simulated = true;
-        }
-
-
-
-        // Reset all inputs at the end
-        pressFlap = false;
-
+        // Handle the rotation of the bird
         HandleRotation();
+
+        // Reset the flap flag to prevent continuous flapping
+        _pressedFlap = false;
+    }
+
+    void ApplyFlap()
+    {
+        // Calculate the force to be applied based on the flap force and current velocity
+        var force = flapForce;
+        force -= RB.velocity.y;
+        RB.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+    }
+
+    void StartSimulating()
+    {
+        // Enable the simulation of the bird's rigidbody and enable rotation
+        RB.simulated = true;
+        _canRotate = true;
+    }
+
+    void HandleRotation()
+    {
+        // Exit early if rotation is not allowed
+        if (!_canRotate) return;
+
+        // Calculate the target rotation based on the bird's velocity
+        var target = maxRotationUpward - transform.rotation.z;
+        var computed = RB.velocity.y / flapForce * target;
+
+        // Smoothly rotate the bird towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, computed), 0.5f);
     }
 
     public void OnFlap(InputAction.CallbackContext context)
     {
-        if (!context.canceled) return;
-        pressFlap = true;
+        // Set the flap flag to true if the flap button was pressed
+        if (!context.started) return;
+        _pressedFlap = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // Handle collisions with the ground
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            _isDead = true;
+            _canRotate = false;
+            _canFlap = false;
+        }
     }
 }
