6c2b7ab0e52d9548ed3f0c52fd6faffad8844187
Wiper-R <rshivang12345@gmail.com>
2023-05-10 15:44:28 +0530

    More progress

--- Assets/Scripts/GroundHandler.cs
+++ Assets/Scripts/GroundHandler.cs
@@ -15,26 +15,30 @@ public class GroundHandler : MonoBehaviour
         groundSprite = ground.GetComponent<SpriteRenderer>();
     }
 
-    Vector2 ComputeSpawnPosition(){
+    Vector2 ComputeSpawnPosition()
+    {
         var lastObject = groundObjects[groundObjects.Count - 1];
         return new Vector2(lastObject.transform.position.x + groundSprite.bounds.size.x, lastObject.transform.position.y);
     }
 
     // Update is called once per frame
 
-    void SpawnGround(Vector2 position){
+    void SpawnGround(Vector2 position)
+    {
         var go = Instantiate(ground, position, Quaternion.identity, gameObject.transform);
         groundObjects.Add(go);
     }
 
 
-    void Update(){
+    void Update()
+    {
         var groundObject = groundObjects[0];
         var pos = new Vector2(groundObject.transform.position.x + groundSprite.bounds.size.x / 2, groundObject.transform.position.y);
         var viewportPoint = mainCamera.WorldToViewportPoint(pos);
 
         // Out of screen
-        if (viewportPoint.x < 0){
+        if (viewportPoint.x < 0)
+        {
             Destroy(groundObject);
             groundObjects.RemoveAt(0);
             SpawnGround(ComputeSpawnPosition());
